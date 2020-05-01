f27233ec08f632b029b721d0ecc5eb559e307e1a
zabir197281 <zabir197281@gmail.com>
2020-05-01 18:31:05 +0600

    Local Settings

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -119,5 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_URL = '/login'
+try:
+    from .local_settings import *
+
+except ImportError:
+    print("Looks like no file.Must me on production.")
